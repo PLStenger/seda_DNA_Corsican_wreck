f3ea9f333033254aa24d43bb952027f5dbe2dfbd
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-18 11:01:16 +0200

    Update and rename readme.md to 01_quality_check_by_FastQC.sh

---
+++ 00_scripts/01_quality_check_by_FastQC.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#SBATCH --job-name=quality_check_by_FastQC
+##SBATCH --time=24:00
+#SBATCH --ntasks=1
+#SBATCH -p gdec
+#SBATCH --nodelist=gdecnode02
+#SBATCH --mem=450G
+#SBATCH -c 32
+#SBATCH --mail-user=pierrelouis.stenger@gmail.com
+#SBATCH --mail-type=ALL 
+#SBATCH --error="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/01_quality_check_by_FastQC.err"
+#SBATCH --output="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/01_quality_check_by_FastQC.out"
+
+# installing FastQC from https://www.bioinformatics.babraham.ac.uk/projects/download.html
+# FastQC v0.11.9 (Mac DMG image)
+
+# Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
+
+WORKING_DIRECTORY=/home/plstenge/seda_DNA_Corsican_wreck/01_raw_data/Fastq
+OUTPUT=/home/plstenge/seda_DNA_Corsican_wreck/02_quality_check
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT
+
+eval "$(conda shell.bash hook)"
+conda activate fastqc
+
+cd $WORKING_DIRECTORY
+
+for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
+do
+      fastqc $FILE -o $OUTPUT
+done ;
+
+conda deactivate fastqc
+conda activate multiqc
+
+# Run multiqc for quality summary
+
+multiqc $OUTPUT
