d1326319c0155599996cdd19ec036bc68765e7b3
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-24 11:49:09 +0200

    Create 06_Komplexity_adapterremoval.sh

---
+++ 00_scripts/06_Komplexity_adapterremoval.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --job-name=06_Komplexity_adapterremoval.
+##SBATCH --time=24:00
+#SBATCH --ntasks=1
+#SBATCH -p smp
+##SBATCH --nodelist=gdecnode02
+#SBATCH --mem=250G
+##SBATCH -c 32
+#SBATCH --mail-user=pierrelouis.stenger@gmail.com
+#SBATCH --mail-type=ALL 
+#SBATCH --error="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/06_Komplexity_adapterremoval.err"
+#SBATCH --output="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/06_Komplexity_adapterremoval.out"
+
+# https://github.com/eclarke/komplexity
+
+WORKING_DIRECTORY=/home/plstenge/seda_DNA_Corsican_wreck/05_bbduk_after_Adapteremoval
+OUTPUT=/home/plstenge/seda_DNA_Corsican_wreck/07_komplexity_Adapteremoval
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT
+
+cd $WORKING_DIRECTORY
+
+# uncompress files
+#gunzip *
+
+# run Komplexity (filtering of low-complexity-reads)
+for sample in UnZipFiles/*truncated ; do kz --filter --threshold 0.55 < $sample > $OUTPUT/$sample.komplex0.55; done 
+
+# compress files
+gzip *komplex0.55* 
