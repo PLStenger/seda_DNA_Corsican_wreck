3c383ce76373ce75bba70a2e6f13bd2ed27af68b
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-24 11:57:11 +0200

    Update 06_Komplexity_adapterremoval.sh

--- 00_scripts/06_Komplexity_adapterremoval.sh
+++ 00_scripts/06_Komplexity_adapterremoval.sh
@@ -26,7 +26,7 @@ cd $WORKING_DIRECTORY
 #gunzip *
 
 # run Komplexity (filtering of low-complexity-reads)
-for sample in UnZipFiles/*truncated ; do kz --filter --threshold 0.55 < $sample > $OUTPUT/$sample.komplex0.55; done 
+for sample in *truncated ; do kz --filter --threshold 0.55 < $sample > $OUTPUT/$sample.komplex0.55; done 
 
 # compress files
 gzip *komplex0.55* 
