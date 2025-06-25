b33c82323207a2b9f6d2913f1e54415b04ad4c13
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-25 13:34:34 +0200

    Update 00_trimmomatic_whole_pipeline.sh

--- 00_scripts/00_trimmomatic_whole_pipeline.sh
+++ 00_scripts/00_trimmomatic_whole_pipeline.sh
@@ -16,12 +16,35 @@ WORKING_DIRECTORY=/home/plstenge/seda_DNA_Corsican_wreck/00_scripts
 
 cd $WORKING_DIRECTORY
 
-sbatch 02_trimmomatic_q30.sh
-sbatch 03_quality_check_by_FastQC_trimmomatic.sh
-sbatch 04_Komplexity_trimmomatic.sh
-sbatch 05_quality_check_after_komplexity_trimmomatic.sh
-sbatch 06_dedupe_trimmomatic.sh
-sbatch 07_quality_check_after_dedup_trimmomatic.sh
-sbatch 08_bbduk_after_Trimmomatic.sh
-sbatch 09_quality_check_after_bbduk_trimmomatic.sh
+# 02_trimmomatic_q30.sh
+#   └─> 03_quality_check_by_FastQC_trimmomatic.sh
+#   └─> 04_Komplexity_trimmomatic.sh
+#         ├─> 05_quality_check_after_komplexity_trimmomatic.sh
+#         └─> 06_dedupe_trimmomatic.sh
+#               ├─> 07_quality_check_after_dedup_trimmomatic.sh
+#               └─> 08_bbduk_after_Trimmomatic.sh
+#                     └─> 09_quality_check_after_bbduk_trimmomatic.sh
 
+# 1. Trimmomatic
+jid1=$(sbatch 02_trimmomatic_q30.sh | awk '{print $4}')
+
+# 2. FastQC after Trimmomatic
+jid2=$(sbatch --dependency=afterok:$jid1 03_quality_check_by_FastQC_trimmomatic.sh | awk '{print $4}')
+
+# 3. Komplexity (depends on Trimmomatic)
+jid3=$(sbatch --dependency=afterok:$jid1 04_Komplexity_trimmomatic.sh | awk '{print $4}')
+
+# 4. FastQC after Komplexity
+jid4=$(sbatch --dependency=afterok:$jid3 05_quality_check_after_komplexity_trimmomatic.sh | awk '{print $4}')
+
+# 5. Dedupe (depends on Komplexity)
+jid5=$(sbatch --dependency=afterok:$jid3 06_dedupe_trimmomatic.sh | awk '{print $4}')
+
+# 6. FastQC after Dedupe
+jid6=$(sbatch --dependency=afterok:$jid5 07_quality_check_after_dedup_trimmomatic.sh | awk '{print $4}')
+
+# 7. BBDuk (depends on Dedupe)
+jid7=$(sbatch --dependency=afterok:$jid5 08_bbduk_after_Trimmomatic.sh | awk '{print $4}')
+
+# 8. FastQC after BBDuk
+jid8=$(sbatch --dependency=afterok:$jid7 09_quality_check_after_bbduk_trimmomatic.sh | awk '{print $4}')
