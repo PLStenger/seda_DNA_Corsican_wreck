36c3f4d60af50fcd98227e5f555add40ed81a2c7
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-25 13:55:31 +0200

    Update 04_Komplexity_adapterremoval.sh

--- 00_scripts/04_Komplexity_adapterremoval.sh
+++ 00_scripts/04_Komplexity_adapterremoval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=06_Komplexity_adapterremoval.
+#SBATCH --job-name=04_Komplexity_adapterremoval.
 ##SBATCH --time=24:00
 #SBATCH --ntasks=1
 #SBATCH -p smp
@@ -9,8 +9,8 @@
 ##SBATCH -c 32
 #SBATCH --mail-user=pierrelouis.stenger@gmail.com
 #SBATCH --mail-type=ALL 
-#SBATCH --error="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/06_Komplexity_adapterremoval.err"
-#SBATCH --output="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/06_Komplexity_adapterremoval.out"
+#SBATCH --error="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/04_Komplexity_adapterremoval.err"
+#SBATCH --output="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/04_Komplexity_adapterremoval.out"
 
 # https://github.com/eclarke/komplexity
 
@@ -26,9 +26,10 @@ cd $WORKING_DIRECTORY
 #gunzip *
 
 # run Komplexity (filtering of low-complexity-reads)
-for sample in *truncated ; do kz --filter --threshold 0.55 < $sample > $OUTPUT/$sample.komplex0.55; done 
+for sample in *truncated ; do kz --filter --threshold 0.55 < $sample > $sample.komplex0.55; done 
 
-cd $OUTPUT
 # compress files
 gzip *komplex0.55* 
+
+scp -r *komplex0.55* $OUTPUT
 
