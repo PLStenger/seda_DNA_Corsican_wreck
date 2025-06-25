bc70109095d19ba0a38054654f44e148694e0aa7
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-25 13:33:02 +0200

    Update 00_adapterremoval_whole_pipeline.sh

--- 00_scripts/00_adapterremoval_whole_pipeline.sh
+++ 00_scripts/00_adapterremoval_whole_pipeline.sh
@@ -16,11 +16,26 @@ WORKING_DIRECTORY=/home/plstenge/seda_DNA_Corsican_wreck/00_scripts
 
 cd $WORKING_DIRECTORY
 
-sbatch 02_adapterremoval.sh
-sbatch 03_quality_check_by_FastQC_adapteremoval.sh
-sbatch 04_Komplexity_adapterremoval.sh
-sbatch 05_quality_check_after_komplexity_adpaterremoval.sh
-sbatch 06_dedupe_adapterremoval.sh
-sbatch 07_quality_check_after_dedup_adapterremoval.sh
-sbatch 08_bbduk_after_Adapteremoval.sh
-sbatch 09_quality_check_after_bbduk_adapterremoval.sh
+# 1. AdapterRemoval
+jid1=$(sbatch 02_adapterremoval.sh | awk '{print $4}')
+
+# 2. FastQC after AdapterRemoval (independent)
+jid2=$(sbatch --dependency=afterok:$jid1 03_quality_check_by_FastQC_adapteremoval.sh | awk '{print $4}')
+
+# 3. Komplexity (depends on AdapterRemoval)
+jid3=$(sbatch --dependency=afterok:$jid1 04_Komplexity_adapterremoval.sh | awk '{print $4}')
+
+# 4. FastQC after Komplexity (depends on Komplexity)
+jid4=$(sbatch --dependency=afterok:$jid3 05_quality_check_after_komplexity_adpaterremoval.sh | awk '{print $4}')
+
+# 5. Dedupe (depends on Komplexity)
+jid5=$(sbatch --dependency=afterok:$jid3 06_dedupe_adapterremoval.sh | awk '{print $4}')
+
+# 6. FastQC after Dedupe (depends on Dedupe)
+jid6=$(sbatch --dependency=afterok:$jid5 07_quality_check_after_dedup_adapterremoval.sh | awk '{print $4}')
+
+# 7. BBDuk (depends on Dedupe)
+jid7=$(sbatch --dependency=afterok:$jid5 08_bbduk_after_Adapteremoval.sh | awk '{print $4}')
+
+# 8. FastQC after BBDuk (depends on BBDuk)
+jid8=$(sbatch --dependency=afterok:$jid7 09_quality_check_after_bbduk_adapterremoval.sh | awk '{print $4}')
