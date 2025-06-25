327decbd08b7eb7854be5c26f53734ea8e71f056
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-25 11:40:44 +0200

    Update 02_trimmomatic_q30.sh

--- 00_scripts/02_trimmomatic_q30.sh
+++ 00_scripts/02_trimmomatic_q30.sh
@@ -29,6 +29,14 @@ conda activate trimmomatic
 
 cd $WORKING_DIRECTORY
 
+# Parameters for non-aDNA:
+# trimmomatic PE -Xmx60G -threads 8 -phred33 $R1 $R2 $OUTPUT/$R1paired $OUTPUT/$R1unpaired $OUTPUT/$R2paired $OUTPUT/$R2unpaired ILLUMINACLIP:"$ADAPTERFILE":2:30:10 LEADING:30 TRAILING:30 SLIDINGWINDOW:26:30 MINLEN:150
+# So the changes are:
+# LEADING:30 Removes bases at the beginning of the read with a quality score < 30 (Phred). → Adaptation for ancient DNA: Trim to LEADING:20 to avoid truncating degraded fragments at the 5' end.
+# TRAILING:30 Removes bases at the end of the read with a score < 30. → Adaptation: Keep TRAILING:30 because degradation of ancient DNA mainly affects the 5' ends.
+# SLIDINGWINDOW:26:30 26-base sliding window: Trim if the average window quality is < 30. → Problem: A wide window (26 bp) can remove small fragments. → Adaptation: Use SLIDINGWINDOW:4:20 for a shorter window (4 bp) and a less stringent threshold (20), thus preserving short fragments.
+# MINLEN:150 Removes reads < 150 bp. → Major problem: ancient DNA in sediments contains short fragments (sometimes < 50 bp). → Crucial adaptation: replace with MINLEN:30 to preserve the small fragments typical of paleogenetic samples.
+
 for R1 in *R1*
 do
    R2=${R1//R1.fastq.gz/R2.fastq.gz}
@@ -37,8 +45,8 @@ do
    R2paired=${R2//.fastq.gz/_paired.fastq.gz}
    R2unpaired=${R2//.fastq.gz/_unpaired.fastq.gz}	
 
-   trimmomatic PE -Xmx60G -threads 8 -phred33 $R1 $R2 $OUTPUT/$R1paired $OUTPUT/$R1unpaired $OUTPUT/$R2paired $OUTPUT/$R2unpaired ILLUMINACLIP:"$ADAPTERFILE":2:30:10 LEADING:30 TRAILING:30 SLIDINGWINDOW:26:30 MINLEN:150
-   
+   trimmomatic PE -Xmx60G -threads 8 -phred33 $R1 $R2 $OUTPUT/$R1paired $OUTPUT/$R1unpaired $OUTPUT/$R2paired $OUTPUT/$R2unpaired ILLUMINACLIP:"$ADAPTERFILE":2:30:10 LEADING:20 TRAILING:30 SLIDINGWINDOW:4:20 MINLEN:30
+
 done ;
 
 
