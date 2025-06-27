f09e965e17683d44622ac9164fce4c227230fd1b
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-27 11:56:29 +0200

    Update and rename 04_Komplexity_adapterremoval.sh to 06_Komplexity_adapterremoval.sh

--- 00_scripts/06_Komplexity_adapterremoval.sh
+++ 00_scripts/06_Komplexity_adapterremoval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=04_Komplexity_adapterremoval.
+#SBATCH --job-name=06_Komplexity_adapterremoval.
 ##SBATCH --time=24:00
 #SBATCH --ntasks=1
 #SBATCH -p smp
@@ -9,13 +9,13 @@
 ##SBATCH -c 32
 #SBATCH --mail-user=pierrelouis.stenger@gmail.com
 #SBATCH --mail-type=ALL 
-#SBATCH --error="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/04_Komplexity_adapterremoval.err"
-#SBATCH --output="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/04_Komplexity_adapterremoval.out"
+#SBATCH --error="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/06_Komplexity_adapterremoval.err"
+#SBATCH --output="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/06_Komplexity_adapterremoval.out"
 
 # https://github.com/eclarke/komplexity
 
-WORKING_DIRECTORY=/home/plstenge/seda_DNA_Corsican_wreck/03_cleaned_data_adapterremoval
-OUTPUT=/home/plstenge/seda_DNA_Corsican_wreck/05_komplexity_adapterremoval
+WORKING_DIRECTORY=/home/plstenge/seda_DNA_Corsican_wreck/05_bbduk_after_Adapteremoval
+OUTPUT=/home/plstenge/seda_DNA_Corsican_wreck/07_komplexity_adapterremoval
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
