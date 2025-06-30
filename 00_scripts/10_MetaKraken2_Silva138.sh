245614500344b813a62fe86170a9aa43190ae1a0
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-30 11:06:10 +0200

    Update 10_MetaKraken2_Silva138.sh

--- 00_scripts/10_MetaKraken2_Silva138.sh
+++ 00_scripts/10_MetaKraken2_Silva138.sh
@@ -12,8 +12,8 @@
 #SBATCH --error="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/10_MetaKraken2_Silva138.err"
 #SBATCH --output="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/10_MetaKraken2_Silva138.out"
 
-INPUT=/home/plstenge/seda_DNA_Corsican_wreck/07_komplexity_Adapteremoval 
-OUTPUT=/home/plstenge/seda_DNA_Corsican_wreck/10_Krona_dedupe_trimmomatic_test
+INPUT=/home/plstenge/seda_DNA_Corsican_wreck/09_dedupe_trimmomatic
+OUTPUT=/home/plstenge/seda_DNA_Corsican_wreck/11_Krona_dedupe_trimmomatic
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
@@ -27,7 +27,7 @@ conda activate MetaKraken2_env
 
 cd $OUTPUT
 
-#gunzip /home/plstenge/seda_DNA_Corsican_wreck/09_dedupe_trimmomatic/clean_1120_sed6_rep3_R1_paired.fastq.komplex0.55.gz-dedupe.gz
+gunzip *-dedupe.gz
 
 for FILE in $(ls $INPUT/*)
 do
@@ -37,4 +37,4 @@ done ;
 #/home/plstenge/MetaKraken2/MetaKraken2.sh -f /home/plstenge/seda_DNA_Corsican_wreck/09_dedupe_trimmomatic/clean_1120_sed6_rep3_R1_paired.fastq.komplex0.55.gz-dedupe  -db /home/plstenge/MetaKraken2/viral_db -c 0.1 -t 2 -o /home/plstenge/seda_DNA_Corsican_wreck/10_Krona_dedupe_trimmomatic
 
 
-mv /home/plstenge/seda_DNA_Corsican_wreck/07_komplexity_Adapteremoval/*kraken2* $OUTPUT
+mv $INPUT/*kraken2* $OUTPUT
