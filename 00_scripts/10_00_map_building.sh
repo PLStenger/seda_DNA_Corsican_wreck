46accb8defdbc8a27d3bce7f032af8ddd336ea3b
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-30 16:37:03 +0200

    Update 10_00_map_building.sh

--- 00_scripts/10_00_map_building.sh
+++ 00_scripts/10_00_map_building.sh
@@ -12,21 +12,26 @@
 #SBATCH --error="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/10_00_map_building.err"
 #SBATCH --output="/home/plstenge/seda_DNA_Corsican_wreck/00_scripts/10_00_map_building.out"
 
-
-# Entrée : FASTA compressé
+# Entrée : fichier FASTA compressé
 FASTA_GZ="/home/plstenge/seda_DNA_Corsican_wreck/99_softwares/SILVA_138.2_SSURef_NR99_tax_silva.fasta.gz"
-# Sortie : fichier acc2taxa.map
 OUTMAP="acc2taxa.map"
 
-# Extraire les headers, parser les IDs et assigner un taxid simulé par hiérarchie
-zgrep "^>" "$FASTA_GZ" | awk -F'[ ;]' '
-{
-  seq_id = substr($1, 2);  # enlève le '>'
-  tax_str = "";
-  for (i=2; i<=NF; i++) {
-    if ($i ~ /;/) break;
-    tax_str = tax_str $i " ";
-  }
-  gsub(/ /, ";", tax_str);
-  print seq_id "\t" tax_str;
+# Vérification du fichier
+if [[ ! -f "$FASTA_GZ" ]]; then
+    echo "Erreur : fichier $FASTA_GZ introuvable"
+    exit 1
+fi
+
+# Extraction + parsing des headers FASTA
+zgrep "^>" "$FASTA_GZ" | awk '{
+    seq_id = substr($1, 2);  # enlève le >
+    tax_str = "";
+    for (i=2; i<=NF; i++) {
+        if ($i ~ /;/) break;
+        tax_str = tax_str $i " ";
+    }
+    gsub(/ /, ";", tax_str);
+    print seq_id "\t" tax_str;
 }' > "$OUTMAP"
+
+echo "Fichier $OUTMAP généré avec succès."
