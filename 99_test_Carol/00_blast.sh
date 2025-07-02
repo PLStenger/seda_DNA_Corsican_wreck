3ab0255f3b65c1e3f1d57058f478db4a1a735ef6
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-07-02 13:54:44 +0200

    Create 00_blast.sh

---
+++ 99_test_Carol/00_blast.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=00_blast
+##SBATCH --time=24:00
+#SBATCH --ntasks=1
+#SBATCH -p smp
+##SBATCH --nodelist=gdecnode02
+#SBATCH --mem=250G
+##SBATCH -c 32
+#SBATCH --mail-user=pierrelouis.stenger@gmail.com
+#SBATCH --mail-type=ALL 
+#SBATCH --error="/home/plstenge/seda_DNA_Corsican_wreck/99_test_Carol/00_blast.err"
+#SBATCH --output="/home/plstenge/seda_DNA_Corsican_wreck/99_test_Carol/00_blast.out"
+
+INPUT=/home/plstenge/99_test_Carol/test.fasta 
+
+cd $INPUT
+
+blastn \
+  -query test.fasta \
+  -db /storage/biodatabanks/ncbi/NT/current/flat/nt \
+  -out results_blastn.tsv \
+  -outfmt 6 \
+  -max_target_seqs 10 \
+  -num_threads 4
